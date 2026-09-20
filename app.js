45012f7ee1937dfd02b751ff3ab9842673d12ed1
thiliniimeshika <thiliniimeshika2003@gmail.com>
2026-09-20 17:07:19 +0530

    increment and decrement operators

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 // console.log("Hello, World!");
 
-let customer={
-    name:"Thilini",
-    age :23,
-    address:"moragahahena",
-    items :["item1","item2","item3"],
+// let customer={
+//     name:"Thilini",
+//     age :23,
+//     address:"moragahahena",
+//     items :["item1","item2","item3"],
 
-    father:{
-        name:"Dad",
-        age:50,
-        address:"Horana"
-    }
+//     father:{
+//         name:"Dad",
+//         age:50,
+//         address:"Horana"
+//     }
 
-}
+// }
 
-//to access fathers properties
-// console.log(customer.father.age);
-
-
+// //to access fathers properties
+// // console.log(customer.father.age);
 
 
-let customer2={
-    items:[
-        {
-            id:1,
-            name:"thilini",
-            variants:[
-                {
-                id:1,
-                name:"dont",
-                price:100
-                }
-            ]
-        },
-        {
-            id:2,
-            name:"imeshika"
-        }
-    ]
-}
+
+
+// let customer2={
+//     items:[
+//         {
+//             id:1,
+//             name:"thilini",
+//             variants:[
+//                 {
+//                 id:1,
+//                 name:"dont",
+//                 price:100
+//                 }
+//             ]
+//         },
+//         {
+//             id:2,
+//             name:"imeshika"
+//         }
+//     ]
+// }
 
 // console.log(customer2.items[1].id);
 
@@ -48,17 +48,28 @@ let customer2={
 
 
 // document.write("<h2>hello world o2</h2>");
-console.log(document.title);
-//this is dom manipulation
+// console.log(document.title);
+// //this is dom manipulation
 
-let heading = document.getElementById("heading");
+// let heading = document.getElementById("heading");
 
 //change the properties of the heading
 
 
-console.log(heading);
+console.log(document.title);
 
-function myFunction(){
-    console.log("button clicked");
-    heading.innerText="Imeshika";
+// function myFunction(){
+//     console.log("button clicked");
+//     heading.innerText="Imeshika" + i;
+// }
+
+let number = 0;
+function increasevalue(){
+  heading.innerText ="Thilini" + number++;
+    console.log("Clicked");
+    
+}
+function decreasevalue(){
+  heading.innerText ="Thilini" + number--;
+    
 }
