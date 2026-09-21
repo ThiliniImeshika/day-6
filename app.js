cf9a21a21f04fd50e4a5df3c70e7c669ceb23f16
thiliniimeshika <thiliniimeshika2003@gmail.com>
2026-09-21 10:23:25 +0530

    take user input to the table(need a correction)

--- app.js
+++ app.js
@@ -1,113 +1,45 @@
-// console.log("Hello, World!");
+let customerList = [];
 
-// let customer={
-//     name:"Thilini",
-//     age :23,
-//     address:"moragahahena",
-//     items :["item1","item2","item3"],
+function btnAddCustomerOnAction() {
+    // Fixed "txtNmae" to "txtName"
+    let txtName = document.getElementById("txtName").value;
+    let txtAge = document.getElementById("txtAge").value;
+    let txtAddress = document.getElementById("txtAddress").value;
+    let txtEmail = document.getElementById("txtEmail").value;
+    let txtSalary = document.getElementById("txtSalary").value; // (Optional: fixed spelling of txtSalay too)
 
-//     father:{
-//         name:"Dad",
-//         age:50,
-//         address:"Horana"
-//     }
+    let customer = {
+        name: txtName,
+        age: txtAge,
+        address: txtAddress,
+        email: txtEmail,
+        salary: txtSalary
+    };
 
-// }
-
-// //to access fathers properties
-// // console.log(customer.father.age);
-
-
-
-
-// let customer2={
-//     items:[
-//         {
-//             id:1,
-//             name:"thilini",
-//             variants:[
-//                 {
-//                 id:1,
-//                 name:"dont",
-//                 price:100
-//                 }
-//             ]
-//         },
-//         {
-//             id:2,
-//             name:"imeshika"
-//         }
-//     ]
-// }
-
-// console.log(customer2.items[1].id);
-
-
-
-//DOM Manipulations(Document Object Model)
-
-
-// document.write("<h2>hello world o2</h2>");
-// console.log(document.title);
-// //this is dom manipulation
-
-// let heading = document.getElementById("heading");
-
-//change the properties of the heading
-
-
-console.log(document.title);
-
-// function myFunction(){
-//     console.log("button clicked");
-//     heading.innerText="Imeshika" + i;
-// }
-
-// let number = 0;
-// function increasevalue(){
-//   heading.innerText ="Thilini" + ++number;
-//     console.log("Clicked");
-    
-// }
-// function decreasevalue(){
-//   heading.innerText ="Thilini" + --number;
-    
-// }
-
-function btnSumOnAction01(){
-
-
-    let n1 =document.getElementById("inputText01").value;
-    let n2 =document.getElementById("inputText02").value;
-
-    //we should convert this to number
-    //let sum =n1 + n2;
-
-    let sum=parseInt(n1) + parseInt(n2);
-
-    console.log(n1);
-    console.log(n2);
-    console.log(sum);
-
-    document.getElementById("heading").innerText="sum:" +sum;
-
+    customerList.push(customer);
+    console.log(customerList);
 }
 
-function btnSumOnAction02(){
-    console.log("hii");
+function loadTable(){
+    let tblCustomers = document.getElementById("btnCustomers");
 
-    let n1 =document.getElementById("inputText01").value;
-    let n2 =document.getElementById("inputText02").value;
+    let body ="";
 
-    //we should convert this to number
-    //let sum =n1 + n2;
+    for(let i=0;i<customerList.length; i++)
+    {
+        body+= `<tr>
+            <td>SAMAN</td>
+            <td>23</td>
+            <td>moragahahena</td>
+            <td>saman@gmail.com</td>
+            <td>78000</td>
+        </tr> `;
 
-    let minus=parseInt(n1) - parseInt(n2);
+        
+    }
 
-    console.log(n1);
-    console.log(n2);
-    console.log(minus);
+    tblCustomers.innerHTML=body;
+ 
 
-    document.getElementById("heading").innerText="minus:" +minus;
-
+    console.log(tblCustomers);
 }
